8dc206bc83f90dc2f457531026e3a3e04c0a10f2
Yura <LavrinovYura@yandex.ru>
2023-03-07 15:22:11 +0300

    CHANGED: backend and fronted были в папках, а не в модулях, поторопился
    STATUS: WORKING

---
+++ back/src/test/java/nic/testproject/accountingsystem/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package nic.testproject.accountingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
