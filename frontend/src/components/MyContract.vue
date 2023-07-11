30ce8b5654860029f6b0f21a6a0191ffcea66812
Anna <evdokimovaanna0016@mail.ru>
2023-07-11 13:51:21 +0300

    вернули контракт обратно

--- frontend/src/components/MyContract.vue
+++ frontend/src/components/MyContract.vue
@@ -9,9 +9,9 @@
             <v-container>
                 <v-row>
                     <v-dialog 
-                        v-model="dialog1"
+                        v-model="$store.state.dialog1"
                         persistent
-                        max-width="600px">
+                        max-width="1000px">
                         <template v-slot:activator="{ on, attrs }"> 
                             <v-btn 
                                 outlined  
@@ -21,113 +21,7 @@
                                     <v-icon >mdi-plus</v-icon>
                             </v-btn>
                         </template>
-                        <v-card>
-                            <v-card-title>
-                                <span class="text-h5"
-                                >Добавить новый контракт
-                                </span>
-                            </v-card-title>
-                            <v-card-text>
-                                <v-text-field 
-                                    label="Название"
-                                    v-model= 'newContract.name'>
-                                </v-text-field>
-                                <v-select
-                                    :items="type" 
-                                    :menu-props="{ top: true, offsetY: true }"
-                                    label="Тип"
-                                    v-model= "newContract.type">
-                                </v-select>
-                                <label>Плановая дата начала
-                                    <v-text-field                                
-                                        type="Date"
-                                        v-model="newContract.plannedStartDate">
-                                    </v-text-field>
-                                </label>
-                                <hr>
-                                <label>Плановая дата окончания
-                                    <v-text-field                                 
-                                        type="Date"
-                                        v-model="newContract.plannedEndDate">
-                                    </v-text-field>
-                                </label>
-                                <hr>
-                                <label>Фактическая дата начала
-                                    <v-text-field                                 
-                                        type="Date"
-                                        v-model="newContract.actualStartDate">
-                                    </v-text-field>
-                                </label>
-                                <hr>
-                                <label>Фактическая дата окончания
-                                    <v-text-field                                 
-                                        type="Date"
-                                        v-model="newContract.actualEndDate">
-                                    </v-text-field>
-                                </label> 
-                                <v-text-field 
-                                    label="Сумма договора"
-                                    v-model="newContract.amount">
-                                </v-text-field>
-                                <v-divider></v-divider>
-                                <section
-                                    
-                                    
-                                    > 
-                                </section>
-                                <section> Организация Контрагент
-                                    <v-row> 
-                                        <v-col><v-text-field  label="name"></v-text-field></v-col>
-                                        <v-col><v-text-field  label="address"></v-text-field></v-col>
-                                        <v-col><v-text-field  label="inn"></v-text-field></v-col>
-                                    </v-row>
-                                    <v-btn outlined  icon color="blue"> 
-                                        <v-icon >mdi-plus</v-icon>
-                                    </v-btn>
-                                </section>
-                            </v-card-text>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn
-                                    color="blue darken-1"
-                                    text
-                                    @click="dialog2 = true"
-                                    >Закрыть
-                                </v-btn>
-                                <v-row>
-                                    <v-dialog width="600px" justify-center
-                                        v-model="dialog2"
-                                        >
-                                        <v-card>
-                                            <v-card-title>
-                                                <div>Вы уверены, что хотите закрыть окно?</div>
-                                                <div>Все несохраненные данные будут утеряны</div>
-                                            </v-card-title>
-                                            <v-card-actions>
-                                                <v-spacer></v-spacer>
-                                                    <v-btn color="blue darken-1"
-                                                        text
-                                                        @click="dialog1 = false"
-                                                        >Закрыть
-                                                    </v-btn>
-                                                    <v-btn color="blue darken-1"
-                                                        text
-                                                        @click="dialog2 = false"
-                                                        >Остаться
-                                                    </v-btn>
-                                            </v-card-actions>
-                                        </v-card>
-                                    </v-dialog>
-                                </v-row>
-                                {{ this.newContract.name }}
-                                <v-btn
-                                    color="blue darken-1"
-                                    text                                
-                                    @click="createNewContract(), sndNewContract(),  getContractt()"
-                                    >Сохранить
-                                </v-btn>
-                            </v-card-actions>
-                        </v-card>
+                        <my-create-contract></my-create-contract>
                     </v-dialog>               
                     <template>
                         <v-btn class="btn"
@@ -149,7 +43,7 @@
                             </th>
                         </thead>
                         <tbody>
-                            <tr v-for="(item, name, index) in contracts"
+                            <tr v-for="(item) in contracts"
                                 :key="item.idndex"
                                 @click="Show(item)"
                                 >
@@ -160,8 +54,8 @@
                                 <td>{{ item.actualStartDate }}</td>
                                 <td>{{ item.actualEndDate }}</td>
                                 <td>{{ item.amount }}</td>
-                                <td>{{ item.phases.length }}</td>
-                                <td>{{ item.contractCounterparties.length }}</td>
+                                <td>{{ item.phases }}</td>
+                                <td>{{ item.contractCounterparties }}</td>
                                 <td>
                                     <router-link class="btn" :to="{name:'contractPage', params: {name: `${item.name}`}}">
                                         <v-btn outlined small depressed>
@@ -186,17 +80,19 @@
             @click="getContractt()">
             download</v-btn>
         </span>  
-        {{ testLocal }}
+        {{ testLocal }}{{ $store.state.tokenType }}
     </v-app>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex';
+import { mapGetters, mapMutations,mapActions } from 'vuex';
 import MyMenu from '../components/MyMenu.vue';
+import MyCreateContract from '../components/MyCreateContract.vue';
 export default {
     name: 'MyContract',
     components: {
         MyMenu,
+        MyCreateContract,
     },
 
     props: {
@@ -204,9 +100,7 @@ export default {
     },
 
     data() { 
-        return {
-            search: '',
-            dialog1: false,
+        return {           
             dialog2: false,
             newContract:
                 {
@@ -217,39 +111,44 @@ export default {
                     actualStartDate: '',
                     actualEndDate: '',
                     amount: '',
-                    phases: [{ }],
-                    contractCounterparties: [{ }]
+                    phases: [{ 
+                        name: '',
+                        amount: '',
+                        planAmount: '',
+                        factAmount: '',
+                        plannedStartDate: '',
+                        plannedEndDate: '',
+                        actualStartDate: '',
+                        actualEndDate: '',
+                    }],
+                    contractCounterparties: [{ 
+                        name: '',
+                        amount: '',
+                        organization: '',
+                        type: '',
+                        plannedStartDate: '',
+                        plannedEndDate: '',
+                        actualStartDate: '',
+                        actualEndDate: '',
+                    }]
                 },
-            allContracts:
-                {
-                    name: '',
-                    type: '',
-                    plannedStartDate: '',
-                    plannedEndDate: '',
-                    actualStartDate: '',
-                    actualEndDate: '',
-                    amount: '',
-                    phases: [{ }],
-                    contractCounterparties: [{ }],
-                    },
-            selected: [],
             testLocal: localStorage.tok,
         }
     },
     computed:{
         ...mapGetters({
-            headers: 'contracts/headers',           
-            contracts: 'contracts/contracts',
-            type: 'contracts/type',
-            stage: 'contracts/stage'
+            headers: 'contractsModul/headers',           
+            contracts: 'contracts',
+            type: 'contractsModul/type',
+        
         }),
         
     },
 
     methods: {
         ...mapMutations({
-            addNewContract: 'contracts/ADD_NEW_CONTRACT',
-            addAllContracts: 'contracts/ADD_ALL_CONTRACTS'
+            addNewContract: 'ADD_NEW_CONTRACT',
+            addAllContracts: 'ADD_ALL_CONTRACTS'
         }),
         sndNewContract() {
             this.dialog1 = false;
@@ -265,60 +164,6 @@ export default {
         },
         Show(item) {
             console.log(item.name)
-        },
-
-        async createNewContract() {
-            try {
-                const response = await axios.post(this.$store.state.url + '/api/menu/contracts/save',
-                {
-                    name: this.newContract.name,
-                    type: "WORKS",
-                    plannedStartDate: '2023-09-15',
-                    plannedEndDate: '2023-09-30',
-                    actualStartDate: '2023-05-01',
-                    actualEndDate: '2023-07-15',
-                    amount: 5000.0,
-                    phases: [
-                        {
-                            name: "Phase 1",
-                            plannedStartDate: "2023-09-15",
-                            plannedEndDate: "2023-09-30",
-                            actualStartDate: "2023-05-01",
-                            actualEndDate: "2023-07-15",
-                            phaseCost: 100000.00,
-                            actualMaterialCosts: 25000.00,
-                            plannedMaterialCosts: 20000.00,
-                            actualSalaryExpenses: 55000.00,
-                            plannedSalaryExpenses: 50000.00
-                        }
-                    ],
-                                    contractCounterparties: [
-                        {
-                            name: "Contract 1",
-                            type: "PROCUREMENT",
-                            counterparty: {
-                                name: "Counterparty 5",
-                                address: "123 Main St",
-                                inn: "555-5555"
-                            },
-                            amount: 100000.0,
-                            plannedStartDate: "2023-04-01",
-                            plannedEndDate: "2023-05-31",
-                            actualStartDate: "2023-05-01",
-                            actualEndDate: "2023-07-15"
-                        }
-                    ],
-                },
-                {headers: {
-                    "Authorization":  "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbm5ubmEifQ.8uH8-TkwiqueuYwOaA7VpFpjQZyvXZbgJWyBc99tN2E",
-                }});
-                console.log(response)
-                
-            } 
-            catch(e) {
-                alert('Неверно')
-                }
-
         },
 
         async getContractt() {
@@ -338,21 +183,12 @@ export default {
                 alert('Error is true')
             }      
         },
-
-        
-
-        
     },
 }
 
 </script>
 
 <style scoped>
-
-.main {
-    max-height: 400px;  
-    max-width: 250px; 
-}
 .content {
     padding-left: 280px;
     padding-top: 20px;
